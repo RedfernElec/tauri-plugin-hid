59daf16b253a7b2430dc0816f5058cdc5b9d912e
Joseph Birks <joseph@redfernelectronics.co.uk>
2025-04-05 13:39:31 +0100

    Initialised Android plugin

---
+++ android/src/main/java/Example.kt
@@ -0,0 +1,10 @@
+package uk.redfern.tauri-plugin-hid
+
+import android.util.Log
+
+class Example {
+    fun pong(value: String): String {
+        Log.i("Pong", value)
+        return value
+    }
+}
